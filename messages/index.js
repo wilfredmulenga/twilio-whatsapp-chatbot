41e56fac14fde91b59b1a9498d791d566d0a34fd
WillzMu <wilfredmulenga@yahoo.com>
2020-03-28 20:00:15 +0200

    test welcome message

---
+++ messages/index.js
@@ -0,0 +1,17 @@
+
+const welcome = 'Welcome to Zambia Covid 19 WhatsApp Portal \n' +
+  'Get information and guidance from WHO and the Ministry of Health regarding the current outbreak of coronavirus disease (COVID-19). \n' +
+  'What would you like to know about coronavirus? \n' +
+  'Reply with a number at any time to get the latest information on the topic: \n' +
+  '1. Latest numbers 🔢 \n' +
+   '2. Protect yourself 👍  \n' +
+  '3. Your questions answered❓ \n' +
+  '4. Mythbusters 🛑 \n' +
+  '5. Travel advice 🗺️ \n' +
+  '6. News & Press 📰 \n' +
+  '7. Share ⏩ \n' +
+  '8. Donate now'
+
+module.exports = {
+  welcome
+}
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,26 @@
 var express = require('express')
 var router = express.Router()
+const messages = require('../messages')
 const { MessagingResponse } = require('twilio').twiml
 
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' })
 })
 
-router.post('/', function (req, res) {
-  console.log('body', req.body)
+function twilioMessage (text) {
+  return new MessagingResponse().message(text)
+}
 
-  const message = new MessagingResponse().message("Thanks for the image! Here's one for you!")
+router.post('/', function (req, res) {
+  const { body } = req
+  let message
+  switch (body) {
+    case 'hey':
+      twilioMessage(messages.welcome)
+      break
+    default:
+      twilioMessage('Sorry, I didnt get that. Send 0 for Menu')
+  }
 
   res.set('Content-Type', 'text/xml')
   res.send(message.toString()).status(200)
