4350184b8ca58fe74c3a0a449b310af96d65fb54
Daryl Lukas <hello@daryllukas.me>
2020-04-04 15:00:17 +0200

    Add Procfile. Remove psql

--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,5 @@
 const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
 const { MessagingResponse } = require('twilio').twiml
-const { pgClient } = require('../helpers/queries')
 
 function handleSendMessage () {
   client.messages
@@ -12,17 +11,6 @@ function handleSendMessage () {
     .then(message => console.log(message.sid))
 }
 
-async function getLatestNumbers () {
-  let result
-  try {
-    result = await pgClient.query('SELECT * from cases')
-  } catch (error) {
-    console.log(error)
-  }
-  const number = result.rows[0].number || ''
-  return twilioMessage(`There are a total of ${number} cases.`)
-}
-
 function twilioMessage (text) {
   const message = new MessagingResponse().message(text)
   return message
@@ -30,6 +18,5 @@ function twilioMessage (text) {
 
 module.exports = {
   handleSendMessage,
-  getLatestNumbers,
   twilioMessage
 }
